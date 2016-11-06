8312980b26da33a02a7c0b216114fcd804451a03
Moises Vilar <moisvv@gmail.com>
2016-11-06 18:01:14 +0100

    Second commit

--- webservice.js
+++ webservice.js
@@ -3,7 +3,10 @@ var server=http.createServer(function(request, response){
        console.log('Knock knock.');
        response.writeHead( 
               200, {"Content-Type": "application/json"});
-       var data = { 'message' : 'Hello Sweetie' };
+       var data = { 
+		'message' : 'Hello Sweetie',
+		'code': 'winki'
+		};
        response.end(JSON.stringify(data) + '\n');
 });
 server.listen(8888);
